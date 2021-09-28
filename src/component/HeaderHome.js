c024dd481f2ffb977ad1914bfedf291e581d924d
rlzmtnck <rlzmtnck@student.telkomuniversity.ac.id>
2021-09-28 19:00:13 +0700

    Logo update

--- src/component/HeaderHome.js
+++ src/component/HeaderHome.js
@@ -5,18 +5,18 @@ import logo from './asset/Daco_4879330.png'
 import 'bootstrap/dist/css/bootstrap.min.css';
 const HeaderHome = () => {
     return (
-        <div className="navhome">
+        <div className="navhome daco">
             <div className="carikbrand carikpos">
                 CaRik
             </div>
-        <div className="logoo">
+        {/* <div className="logoo">
             <img
           src={ logo }
           width="489"
           height="476"
           alt=""
         />
-    </div>
+    </div> */}
         </div>
     )
 }
