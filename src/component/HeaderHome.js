db8f28a9b7746b242ecac61bc6d3089a0d63e95d
rlzmtnck <rlzmtnck@student.telkomuniversity.ac.id>
2021-09-28 17:32:57 +0700

    Beginning

---
+++ src/component/HeaderHome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './kepala.css'
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+const HeaderHome = () => {
+    return (
+        <div className="navhome">
+            <div className="carikbrand carikpos">
+                CaRik
+            </div>
+        </div>
+    )
+}
+export default HeaderHome
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeaderBiasa from '../component/HeaderBiasa';
+import HeaderHome from '../component/HeaderHome';
+
+const Home = () => {
+    return (
+        // <HeaderBiasa/>
+           <HeaderHome/>
+    )
+
+    
+}
+export default Home
