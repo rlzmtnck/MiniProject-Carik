6520d18bdeba181b6423680869541c9ddf87c147
rlzmtnck <rlzmtnck@student.telkomuniversity.ac.id>
2021-09-28 18:30:03 +0700

    gambar

--- src/component/HeaderHome.js
+++ src/component/HeaderHome.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './kepala.css'
-
+import logo from './asset/Daco_4879330.png'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 const HeaderHome = () => {
@@ -9,6 +9,14 @@ const HeaderHome = () => {
             <div className="carikbrand carikpos">
                 CaRik
             </div>
+        <div className="logoo">
+            <img
+          src={ logo }
+          width="489"
+          height="476"
+          alt=""
+        />
+    </div>
         </div>
     )
 }
